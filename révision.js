b7a491e0d25b12d20324e9b5dda9e138eff01e03
phung <seb111.penna@gmail.com>
2018-08-26 21:28:37 +0200

    first commit

---
+++ révision.js
@@ -0,0 +1,35 @@
+var addition = function (a,b) {
+		return a+b;
+}
+
+var soustraction = function (a,b) {
+	return a-b;
+}
+
+var conversion = function (kg) {
+	var gramme = kg * 1000;
+	return gramme;
+}
+
+var hero = {
+	name:'toto',
+	hp: 450,
+}
+
+var addHP = function (hp,name) {
+
+	return name "a" + somme(hp,hero.hp) + "hp";
+	
+}
+
+
+var array = [2,2,3,1];
+
+
+var arrayAddition = function (a) {
+	for ( let i = 0, i < array.length, i++) {
+		//var b = array[i];
+		var c = array[i] 
+		return c ;
+	}
+}
